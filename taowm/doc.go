8b224a50e9dd870c6fe98a7bc7078bdc326fe4ab
Nigel Tao <nigeltao@golang.org>
2015-04-01 20:38:55 +1100

    taowm: fix doc for the screen-lock combo becoming Caps+Shift+|.
    TBR=nigeltao
    R=nigeltao
    CC=taowm
    https://codereview.appspot.com/217600043

--- taowm/doc.go
+++ taowm/doc.go
@@ -43,10 +43,10 @@ default Caps Lock behavior, CHANGING ALL TYPED LETTERS TO UPPER CASE, is
 disabled.
 
 Caps Lock and the Space key will open a new web browser window. Caps Lock and
-the Enter key will open a new terminal emulator window. Caps Lock and the '\'
-backslash key will lock the screen. Caps Lock and the Backspace key will close
-the window in the focused frame. Caps Lock and the Tab key will cycle through
-the frames.
+the Enter key will open a new terminal emulator window. Caps Lock and the Shift
+key and the '|' pipe key will lock the screen. Caps Lock and the Backspace key
+will close the window in the focused frame. Caps Lock and the Tab key will
+cycle through the frames.
 
 To quit taowm and return to the log in screen, hold down Caps Lock and the
 Shift key and hit the Escape key three times in quick succession. Normally,
